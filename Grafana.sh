d52e00f4db52e9298b618f5de210c8013f4cd364
sajitbhaskaran <54746076+sajitbhaskaran@users.noreply.github.com>
2024-05-26 12:36:57 +0530

    Create Grafana.sh

---
+++ Grafana.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.0.0_amd64.deb
+sudo dpkg -i grafana-enterprise_11.0.0_amd64.deb
