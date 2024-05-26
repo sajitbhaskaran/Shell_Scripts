465681da6e8858de53ca54aa0391fd7663f419aa
sajitbhaskaran <54746076+sajitbhaskaran@users.noreply.github.com>
2024-05-26 12:39:38 +0530

    Update Grafana.sh

--- Grafana.sh
+++ Grafana.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.0.0_amd64.deb
 sudo dpkg -i grafana-enterprise_11.0.0_amd64.deb
+sudo /bin/systemctl start grafana-server
